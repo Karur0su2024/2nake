615ae151f40c9829bb17b2e55407de903aeb970f
Karur0su <50730505+Karur0su2024@users.noreply.github.com>
2024-06-10 18:57:40 +0200

    Base game

--- client/src/main/java/org/example/Game.java
+++ client/src/main/java/org/example/Game.java
@@ -1,2 +1,18 @@
-package org.example;public class Game {
+package org.example;
+
+import javax.swing.*;
+
+public class Game extends JFrame {
+
+    Game(){
+        this.add(new Panel());
+        this.setTitle("Snake");
+        this.setResizable(false);
+        this.pack();
+        this.setVisible(true);
+        this.setLocationRelativeTo(null);
+    }
+
+
+
 }

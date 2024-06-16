e753388168b2711df2aba5f44de0a1a731e4de05
Karur0su <50730505+Karur0su2024@users.noreply.github.com>
2024-06-16 22:04:29 +0200

    Game succesfully works!

---
+++ server/src/main/java/org/example/Main.java
@@ -0,0 +1,16 @@
+package org.example;
+
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class Main {
+    private static final Logger log = LoggerFactory.getLogger(Main.class);
+
+    public static void main(String[] args) {
+
+        GameServer server = new GameServer();
+
+
+    }
+}

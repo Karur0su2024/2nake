07bc3239b2889dce1b4b9960c28400d2913b9120
Karur0su <50730505+Karur0su2024@users.noreply.github.com>
2024-06-23 12:33:07 +0200

    improved game optimalization

--- server/src/main/java/org/example/GameServer.java
+++ server/src/main/java/org/example/GameServer.java
@@ -4,58 +4,68 @@ import org.example.objects.Snake;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
 import java.io.IOException;
 import java.net.ServerSocket;
-import java.net.Socket;
-import java.util.HashSet;
-import java.util.Set;
-import javax.swing.*;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
 
 /**
- * Třída GameServer představuje serverovou část hry, která spravuje spojení s klienty,
- * inicializuje a řídí průběh hry a komunikuje s klienty přes síť.
+ * The GameServer class represents the server part of the game, managing client connections,
+ * initializing and controlling the game flow, and communicating with clients over the network.
  */
-public class GameServer implements ActionListener, Runnable {
+public class GameServer implements Runnable {
 
-    private static final Logger log = LoggerFactory.getLogger(Main.class);
-    private ClientHandler[] clientHandlers;
-    private boolean alive = true;
-
-
+    private static final Logger log = LoggerFactory.getLogger(GameServer.class);
+    private final ClientHandler[] clientHandlers;
+    private volatile boolean alive = true;
     private int time;
-    private Timer timer;
-
     private Game game;
     private ServerSocket serverSocket;
+    private final ScheduledExecutorService scheduler;
 
-
+    /**
+     * Constructor to create an instance of GameServer that listens on the specified port.
+     * Starts the server and waits for client connections. After two clients are connected, starts the game.
+     *
+     * @param clientHandlers array of ClientHandler instances representing connected clients
+     */
     public GameServer(ClientHandler[] clientHandlers) {
         this.clientHandlers = clientHandlers;
+        this.scheduler = Executors.newScheduledThreadPool(1);
 
+        startGame();
+        initializeClients();
     }
 
     /**
-     * Spouští hru s přednastavenými parametry a inicializuje herní stav.
-     * Informuje klienty o začátku hry a spouští herní časovač pro pravidelnou aktualizaci hry.
+     * Initializes the game and sets up clients with their respective game and player index.
+     */
+    private void initializeClients() {
+        for (int i = 0; i < clientHandlers.length; i++) {
+            clientHandlers[i].setGame(game);
+            clientHandlers[i].setPlayer(i);
+        }
+    }
+
+    /**
+     * Starts the game with predefined parameters and initializes the game state.
+     * Informs clients about the start of the game and schedules the game updates.
      */
     private void startGame() {
         time = 0;
-        timer = new Timer(5, this);
-        game = new Game(2, 60, 50, 20, 10, 6, 240); // Přednastavené parametry pro hru
-
+        game = new Game(2, 60, 50, 20, 10, 6, 240); // Predefined game parameters
         broadcastMessage("start");
-        log.info("Začátek hry");
+        log.info("Game started");
         game.startGame();
-        timer.start();
+        scheduler.scheduleAtFixedRate(this, 5, 25, TimeUnit.MILLISECONDS);
         broadcastGame();
     }
 
     /**
-     * Odesílá zprávu všem klientům připojeným k serveru.
+     * Sends a message to all clients connected to the server.
      *
-     * @param message zpráva k odeslání
+     * @param message message to send
      */
     private synchronized void broadcastMessage(String message) {
         for (ClientHandler client : clientHandlers) {
@@ -64,7 +74,7 @@ public class GameServer implements ActionListener, Runnable {
     }
 
     /**
-     * Odesílá aktuální stav hry všem klientům připojeným k serveru.
+     * Sends the current game state to all clients connected to the server.
      */
     private synchronized void broadcastGame() {
         String gameState = game.toString();
@@ -74,21 +84,23 @@ public class GameServer implements ActionListener, Runnable {
     }
 
     /**
-     * Metoda actionPerformed pro obsluhu události akce časovače.
-     * Aktualizuje stav hry v pravidelných intervalech a odesílá aktualizovaný stav všem klientům.
-     *
-     * @param e událost akce
+     * Handles the game update logic, updating the game state at regular intervals and
+     * sending the updated state to all clients.
      */
     @Override
-    public void actionPerformed(ActionEvent e) {
+    public void run() {
+        if (!alive) {
+            return;
+        }
+
         time++;
         synchronized (game) {
             if (game.isRunning()) {
-                if (time % 100 * game.getPlayers() == 0) {
+                if (time % (100 * game.getPlayers()) == 0) {
                     game.generateAction();
                 }
 
-                if (time % 40 * game.getPlayers() == 0) {
+                if (time % (40 * game.getPlayers()) == 0) {
                     game.decreaseTime();
                 }
 
@@ -97,23 +109,34 @@ public class GameServer implements ActionListener, Runnable {
                         snake.move();
                         game.checkCollisions();
                         game.checkFood();
+                        System.out.println(game.toString());
                     }
                     if (game.getTime() == 0) {
                         game.setRunning(false);
-                        log.info("Konec hry");
+                        log.info("Game ended");
                     }
                 }
             }
             broadcastGame();
         }
+
+        // Check if any client handlers are not alive
+        for (ClientHandler ch : clientHandlers) {
+            if (!ch.isAlive()) {
+                alive = false;
+                scheduler.shutdown();
+                log.info("Shutting down game server due to client disconnection.");
+                break;
+            }
+        }
     }
 
     /**
-     * Ukončí běh serveru, uzavře socket a ukončí všechna spojení s klienty.
+     * Terminates the server, closes the server socket, and ends all client connections.
      */
     public void terminate() {
         alive = false;
-        timer.stop();
+        scheduler.shutdown();
         try {
             if (serverSocket != null) {
                 serverSocket.close();
@@ -122,27 +145,8 @@ public class GameServer implements ActionListener, Runnable {
                 clientHandler.closeConnection();
             }
         } catch (IOException e) {
-            log.error("Error during server termination: " + e);
+            log.error("Error during server termination: ", e);
         }
         log.info("Server stopped.");
-    }
-
-    @Override
-    public void run() {
-        startGame();
-
-        for(int i = 0; i < clientHandlers.length; i++){
-            clientHandlers[i].setGame(game);
-            clientHandlers[i].setPlayer(i);
-        }
-
-        while (alive){
-            for(ClientHandler ch : clientHandlers){
-                if(!ch.isAlive()){
-                    alive = false;
-                }
-            }
-        }
-        timer.stop();
     }
 }
